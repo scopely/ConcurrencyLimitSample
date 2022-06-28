7a851a4267160f5e0c521ee89bbd4e7f10832ba8
Brandon Cuff <brandon@scopely.com>
2022-06-28 10:31:15 -0700

    initial commit

---
+++ ConcurrencyLimitMiddleware.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConcurrencyLimitSample
+{
+    public static class ConcurrencyLimitMiddleware
+    {
+        public static IApplicationBuilder UseConcurrencyLimit(this IApplicationBuilder app, int maxConcurrency, TimeSpan waitTimeout)
+        {
+            var sem = new SemaphoreSlim(maxConcurrency);
+            var reportTimer = new Timer(o =>
+            {
+                // report the number of requests in flight once per second
+                MetricReporter.Gauge("requests.concurrent", sem.CurrentCount);
+            }, null, TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(1));
+            return app.Use(async (context, next) =>
+            {
+                if (!await sem.WaitAsync(waitTimeout))
+                {
+                    // reject requests outright if it takes too long to get a concurrency slot
+                    // also, note that the client can check for this status code and
+                    // retry the request with some exponential backoff
+                    context.Response.StatusCode = 503;
+                    context.Response.ContentType = "text/plain";
+                    await context.Response.WriteAsync("Service Unavailable");
+                    return;
+                }
+                try
+                {
+                    await next();
+                }
+                finally
+                {
+                    sem.Release();
+                }
+            });
+        }
+    }
+}
---
+++ MetricReporter.cs
@@ -0,0 +1,12 @@
+﻿namespace ConcurrencyLimitSample
+{
+    public class MetricReporter
+    {
+        public static void Gauge(string name, double value)
+        {
+            // todo - implement
+            // not that we use datadog - See their docs on Gauge metrics
+            // https://docs.datadoghq.com/metrics/types/?tab=gauge&tabs=gauge#metric-types
+        }
+    }
+}
